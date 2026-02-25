d8c7faf1d9c1d3fa269abd521c03e62a148cbff1
shushi93 <shashi93@gmail.com>
2026-02-25 15:04:12 +0800

    made resourcepack button redirect to my own screen

--- src/main/java/resource/modid/mixin/ResourceScreenMixin.java
+++ src/main/java/resource/modid/mixin/ResourceScreenMixin.java
@@ -2,13 +2,16 @@ package resource.modid.mixin;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.options.OptionsScreen;
-import net.minecraft.client.gui.screens.packs.PackSelectionScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import resource.modid.gui.ExampleGui;
+import resource.modid.gui.ExampleScreen;
 
 import java.util.function.Supplier;
 
@@ -22,8 +25,9 @@ public abstract class ResourceScreenMixin extends Screen {
     @Definition(id = "openScreenButton", method = "Lnet/minecraft/client/gui/screens/options/OptionsScreen;openScreenButton(Lnet/minecraft/network/chat/Component;Ljava/util/function/Supplier;)Lnet/minecraft/client/gui/components/Button;")
     @Definition(id = "RESOURCEPACK", field = "Lnet/minecraft/client/gui/screens/options/OptionsScreen;RESOURCEPACK:Lnet/minecraft/network/chat/Component;")
     @Expression("this.openScreenButton(RESOURCEPACK, ?)")
-    @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "MIXINEXTRAS:EXPRESSION")))
-    private Supplier<Screen> redirectButton(Component component, Supplier<Screen> supplier){
+    @ModifyArg(method = "init", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private Supplier redirectButton(Supplier supplier){
+        supplier = () -> new ExampleScreen(new ExampleGui());
         return supplier;
     }
 }
