d245fbf2fea0b5a435278ae5268a10ee2c8e4699
shushi93 <shashi93@gmail.com>
2026-08-21 15:58:27 +0800

    set to dev env, made buttons actl do smth

--- src/client/java/net/shushi93/resource/client/gui/screens/TextureScreen.java
+++ src/client/java/net/shushi93/resource/client/gui/screens/TextureScreen.java
@@ -8,7 +8,14 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.shushi93.resource.ResourceScreening;
 import net.shushi93.resource.client.gui.widget.DropdownList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The main mod screen
@@ -16,7 +23,11 @@ import net.shushi93.resource.client.gui.widget.DropdownList;
 @Environment(EnvType.CLIENT)
 public class TextureScreen extends Screen {
     public static final int RETURN_LOCATION = 172;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceScreening.MOD_ID);
     public final Screen parent;
+    private final Minecraft mc = Minecraft.getInstance();
+    private final PackRepository pr = mc.getResourcePackRepository();
+    private final List<String> hacking_noises = new ArrayList<>(pr.getAvailableIds());
 
     public TextureScreen(Component title, Screen parent) {
         super(title);
@@ -37,11 +48,13 @@ public class TextureScreen extends Screen {
      */
     @Override
     protected void init() {
+//        LOGGER.info("Available IDs: {}", pr.getAvailableIds());
+//        LOGGER.info("Selected IDs: {}", pr.getSelectedIds());
         EditBox search = new EditBox(this.font, 40, 40 - this.font.lineHeight, 350, 20, Component.empty());
 
 //        SpriteIconButton spriteIconButton2 = this.addRenderableWidget(
 //                filter.filter_btn(
-//                        20, b -> Minecraft.getInstance().setScreen(new TextureFilterScreen(Component.empty(), this)), true
+//                        20, b -> mc.setScreen(new TextureFilterScreen(Component.empty(), this)), true
 //                )
 //        );
 //        spriteIconButton2.setPosition(400, 40 - this.font.lineHeight);
@@ -50,11 +63,15 @@ public class TextureScreen extends Screen {
         DropdownList dropdown = new DropdownList(172, 112, 120, 20);
 
         Button back = Button.builder(Component.translatable("gui.screens.TextureScreen.backButton"), (b) -> onClose()).bounds(TextureScreen.RETURN_LOCATION, 224, 120, 20).build();
-        Button b1 = Button.builder(Component.literal("B1"), b -> onClose()).bounds(120, 112, 20, 20).build();
-        Button b2 = Button.builder(Component.literal("B2"), b -> onClose()).bounds(180, 112, 20, 20).build();
+        Button b1 = Button.builder(Component.literal("B1"), b -> {
+            onClick("doogile");
+        }).bounds(120, 112, 20, 20).build();
+        Button b2 = Button.builder(Component.literal("B2"), b -> {
+            onClick("riley");
+        }).bounds(120, 144, 20, 20).build();
 
-        this.addRenderableOnly(b1);
-        this.addRenderableOnly(b2);
+        this.addRenderableWidget(b1);
+        this.addRenderableWidget(b2);
         this.addRenderableWidget(search);
         this.addRenderableWidget(back);
         //this.addRenderableWidget(dropdown);
@@ -62,6 +79,19 @@ public class TextureScreen extends Screen {
 
     @Override
     public void onClose() {
-        Minecraft.getInstance().setScreen(this.parent);
+        mc.setScreen(this.parent);
+    }
+
+    private void reset_hacking_noises() {
+        hacking_noises.clear();
+        hacking_noises.addAll(pr.getAvailableIds());
+    }
+
+    private void onClick(String name) {
+        reset_hacking_noises();
+        hacking_noises.add(name);
+        LOGGER.info("Available IDs: {}", hacking_noises);
+        pr.setSelected(hacking_noises);
+        mc.reloadResourcePacks();
     }
 }
